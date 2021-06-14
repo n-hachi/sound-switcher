68e44155749c6a77e98690456d47061c653c68a2
Nozomu Hatta <nozomuhatta@gmail.com>
2021-06-15 00:18:48 +0900

    Build simple library

---
+++ include/sound/lib.hpp
@@ -0,0 +1,12 @@
+#ifndef SAMPLE_CLIENT_INCLUDE_SOUND_LIB_HPP
+#define SAMPLE_CLIENT_INCLUDE_SOUND_LIB_HPP
+
+class SoundSwitcher {
+   public:
+    SoundSwitcher();
+    void Start();
+
+   private:
+};
+
+#endif
---
+++ src/lib.cpp
@@ -0,0 +1,3 @@
+#include "sound/lib.hpp"
+
+SoundSwitcher::SoundSwitcher(){};
