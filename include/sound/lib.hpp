df99b77a7cd68fc8bcc9266c4e42344adc786477
Nozomu Hatta <nozomuhatta@gmail.com>
2021-06-18 00:55:02 +0900

    WIP: Add Start and Stop method to SoundSwitcher

--- include/sound/lib.hpp
+++ include/sound/lib.hpp
@@ -1,12 +1,22 @@
 #ifndef SAMPLE_CLIENT_INCLUDE_SOUND_LIB_HPP
 #define SAMPLE_CLIENT_INCLUDE_SOUND_LIB_HPP
 
+#include <sys/types.h>
+
+#include <mutex>
+#include <thread>
+
 class SoundSwitcher {
    public:
     SoundSwitcher();
     void Start();
+    void Stop();
 
    private:
+    // Need to be not public
+    pid_t pid_ = -1;
+    bool is_running_ = false;
+    std::thread th_;
 };
 
 #endif
--- src/lib.cpp
+++ src/lib.cpp
@@ -1,3 +1,41 @@
 #include "sound/lib.hpp"
 
-SoundSwitcher::SoundSwitcher(){};
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <iostream>
+
+void SoundSwitcher::Stop() {
+    // If pid_ is less equal than 0, there is no child process.
+    if (pid_ <= 0) {
+        return;
+    }
+
+    // If pid is more than 0, stop child process.
+    kill(pid_, SIGTERM);
+    waitpid(pid_, NULL, 0);
+}
+
+void SoundSwitcher::Start() {
+    // If child process has already executed, stop it at first.
+    Stop();
+
+    pid_ = vfork();
+    if (pid_ == -1) {
+        std::cerr << "Error: vfork" << std::endl;
+        return;
+    }
+    if (pid_ == 0) {
+        char *const args[] = {
+            "/usr/bin/aplay",
+            "/home/n_hachi/src/github.com/n-hachi/sound-switcher/sounds/"
+            "mixkit-retro-game-emergency-alarm-1000.wav",
+            ""};
+        execvp(args[0], args);
+    }
+    if (pid_ > 0) {
+        waitpid(pid_, NULL, 0);
+    }
+}
