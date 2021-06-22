bd8207fd443cf61799b849e5bb7a72add865427e
Nozomu Hatta <nozomuhatta@gmail.com>
2021-06-22 17:24:24 +0900

    Add sample prompt

--- app/main.cpp
+++ app/main.cpp
@@ -3,8 +3,10 @@
 #include <chrono>
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <thread>
+#include <vector>
 
 #include "sound/lib.hpp"
 #include "version.h"
@@ -46,11 +48,39 @@ int main(int argc, char* argv[]) {
     SoundSwitcher switcher;
     for (int i = optind; i < argc; ++i) {
         std::string filename = argv[i];
-
-        // Debug
-        std::cout << "file : " << filename << std::endl;
-
         switcher.Insert(filename);
+    }
+
+    while (true) {
+        // Print prompt
+        std::cout << "> ";
+        std::string line;
+        std::getline(std::cin, line);
+
+        std::istringstream ss(line);
+        std::string token;
+        std::vector<std::string> tokens;
+        while (ss >> token) {
+            tokens.push_back(token);
+        }
+
+        if (tokens[0] == "start") {
+            if (tokens.size() < 2) {
+                std::cout << "input 'start <number>' , 'stop' or 'quit'"
+                          << std::endl;
+                continue;
+            }
+            int num = std::stoi(tokens[1]);
+            // Just for debug
+            std::cout << "command = 'start' , val = " << num << std::endl;
+        } else if (tokens[0] == "stop") {
+            std::cout << "command = 'stop'" << std::endl;
+        } else if (tokens[0] == "quit") {
+            break;
+        } else {
+            std::cout << "input 'start <number>' , 'stop' or 'quit'"
+                      << std::endl;
+        }
     }
 
     switcher.Start();
